d7a2bd3aeda1522f6fa5fa09196432ad52484fb3
Crt Gorican <crt.gorican@gmail.com>
2026-01-12 22:11:05 +0100

    qol improvements

--- src/cec2014/cec14.rs
+++ src/cec2014/cec14.rs
@@ -43,7 +43,7 @@ impl Cec14 {
             panic!("Test functions are only defined for D=2,10,20,30,50,100.");
         }
         let problem_index = self.problem.index();
-        if self.dim==2&&((problem_index >= 17 && problem_index <= 22) || (problem_index >= 29 && problem_index <= 30))
+        if self.dim == 2 && ((problem_index >= 17 && problem_index <= 22) || (problem_index >= 29 && problem_index <= 30))
         {
             panic!("Error: hf01,hf02,hf03,hf04,hf05,hf06,cf07,cf08 are NOT defined for D=2.");
         }
@@ -53,18 +53,16 @@ impl Cec14 {
         self.s = helper.load_shuffle_vector(problem_index, self.dim);
     }
 
-    #[allow(dead_code)]
-    pub fn set_dim(&mut self, dim: usize) {
-        if !AVAILABLE_DIM.contains(&dim)
-        {
-            panic!("Test functions are only defined for D=2,10,20,30,50,100.");
-        }
-        self.dim = dim;
-        self.load_input_data(&Cec14Helper::default());
+    pub fn get_dim(&self) -> usize {
+        self.dim
     }
 
-    #[allow(dead_code)]
-    pub fn set_problem(&mut self, problem: Cec14Function) {
+    pub fn get_problem(&self) -> Cec14Function {
+        self.problem.clone()
+    }
+
+    pub fn set_problem(&mut self, problem: Cec14Function, dim: usize) {
+        self.dim = dim;
         self.problem = problem;
         self.load_input_data(&Cec14Helper::default());
     }
